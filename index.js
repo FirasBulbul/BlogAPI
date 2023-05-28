0f5200a292a2de74761add6a9f626c8c41e46e68
Firas Abdulrahman <106191320+FirasBulbul@users.noreply.github.com>
2023-05-28 19:26:20 +0300

    ejs rendering

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const PORT = 5000 || process.env.PORT
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 
 const app = express();
 
-app.get('', (req, res, next) => {
-    res.send('Hello World !!');
-});
+app.use(express.static('Public'));
+
+
+//Template Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+
+app.use('/', require('./Routes/index.js'));
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
